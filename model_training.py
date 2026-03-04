c66086ba05f9f325dff47af20e19f5f7cf2f50b6
ManuelCabral98 <espinola.alejandro09@gmail.com>
2026-03-03 23:23:29 -0300

    Configuration and transformation of the dataset.

---
+++ model_training.py
@@ -0,0 +1,30 @@
+from torchvision import transforms, datasets
+from torchvision.models import ResNet18_Weights
+from torch.utils.data import DataLoader, random_split
+
+# TRADITIONAL WAY TO TRANSFORM DATASETS INTO 'RESNET18' LEGIBLE DATASET
+# mean=[0.485, 0.456, 0.406]
+# std=[0.229, 0.224, 0.225]
+
+# images_transforms = transforms.Compose([
+#     transforms.Resize((224, 224)),
+#     transforms.ToTensor(),
+#     transforms.Normalize(mean, std)
+# ])
+
+# AUTOMATIC WAY INTRODUCED BY PYTORCH
+my_transforms = ResNet18_Weights.DEFAULT.transforms()
+
+# READ MY 'ANIMALS' FOLDER TO SEPARATE THE OBJECTS IN IT, BESIDES THAT IT 'TRANSFORM' THE IMAGES WITH THE CONFIGURATION 
+# WE SET IN THE PREVIOUS VARIABLE (my_transforms)
+dataset = datasets.ImageFolder("C:\Users\alejandro\Desktop\projects\deep_learning_model\animals", transform=my_transforms)
+
+# SPLIT THE DATASET IN 2 PARTS, ONE FOR TRAINING AND THE OTHER ONE FOR VALIDATION
+train_size = int(len(dataset) * 0.8)
+validation_size = int(len(dataset) - train_size)
+
+train_data, validation_data = random_split(dataset, [train_size, validation_size])
+
+train_loader = DataLoader(train_data, batch_size=32, shuffle=True)
+validation_loader = DataLoader(validation_data, batch_size=32, shuffle=False)
+
