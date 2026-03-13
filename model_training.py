c5e19ba4ee06bf1e6d5ce11537070715689d7f49
ManuelCabral98 <espinola.alejandro09@gmail.com>
2026-03-13 14:31:23 -0300

    Redefining last layer of the model and establishing the device to use (gpu or cpu)

--- model_training.py
+++ model_training.py
@@ -1,7 +1,14 @@
-from torchvision import transforms, datasets
-from torchvision.models import ResNet18_Weights
-from torch.utils.data import DataLoader, random_split
+# LIBRARY FOR HANDLE DYNAMIC PATH
 from pathlib import Path
+# RESNET ECOSYSTEM EXPERT IN IMAGES
+from torchvision import transforms, datasets
+from torchvision.models import ResNet18_Weights, resnet18
+# TOOLS FOR LOADING AND SPLITING IMAGES
+from torch.utils.data import DataLoader, random_split
+# PYTORCH CORE
+import torch
+import torch.nn as nn
+
 
 # TRADITIONAL WAY TO TRANSFORM DATASETS INTO 'RESNET18' LEGIBLE DATASET
 # mean=[0.485, 0.456, 0.406]
@@ -35,3 +42,28 @@ train_data, validation_data = random_split(dataset, [train_size, validation_size
 train_loader = DataLoader(train_data, batch_size=32, shuffle=True)
 validation_loader = DataLoader(validation_data, batch_size=32, shuffle=False)
 
+# INSTANTIANTING THE MODEL
+model = resnet18(ResNet18_Weights.DEFAULT)
+
+# FREEZEING THE MODEL TO AVOID 'TRAINING' OR 'LEARNING'
+# OUR GOAL ISN'T TO IMPROVE PARAMETERS' GRADIENTS, BECAUSE MODEL IS ALREADY TRAINED
+for parameter in model.parameters():
+    parameter.requires_grad = False
+
+# REPLACING 'FULLY CONNECTED' (FC) LAYER FOR ONLY 5 OUT FEATURES
+# DEFAULT RESNET18 MODEL HAS 1000 OUT FEATURES AND LOTS OF CATEGORIES (DOGS, CATS, ETC...)
+# WE ONLY NEED 5 FOR OUR DATASET
+in_features_qty = model.fc.in_features
+model.fc = nn.Linear(in_features=in_features_qty, out_features=5)
+
+# LOOKING FOR MACHINE COMPONENT TO WORK WITH
+if torch.cuda.is_available():
+    device = torch.device("cuda")
+elif torch.backends.mps.is_available(): # FOR Macs WITH M1/M2/M3 CHIPS
+    device = torch.device("mps")
+else:
+    device = torch.device("cpu")
+
+# UPLOADING MODEL UP TO SELECTED DEVICE
+# GOLD RULE: DATASET AND MODEL MUST BE IN THE SAME DEVICE
+model = model.to(device)
